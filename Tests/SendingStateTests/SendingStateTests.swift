95477d8f120a1512c96fbc63da1f6f851424c367
Sunny Jeon <dSunny90@users.noreply.github.com>
2020-11-21 16:45:18 +0900

    Initial Commit

---
+++ Tests/SendingStateTests/SendingStateTests.swift
@@ -0,0 +1,12 @@
+//
+//  SendingStateTests.swift
+//  SendingState
+//
+//  Created by SunSoo Jeon on 2020/11/20.
+//
+
+import XCTest
+@testable import SendingState
+
+final class SendingStateTests: XCTestCase {
+}
