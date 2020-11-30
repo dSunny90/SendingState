52593987f0102fdad398c499ad93564c628aca04
Sunny Jeon <dSunny90@users.noreply.github.com>
2020-11-30 23:05:32 +0900

    Expand XCTest coverage for ConfigurableTests

--- Tests/SendingStateTests/ConfigurableTests/ConfigurableTests.swift
+++ Tests/SendingStateTests/ConfigurableTests/ConfigurableTests.swift
@@ -30,3 +30,94 @@ final class ConfigurableTests: XCTestCase {
         XCTAssertEqual(controller.value, "Hello, World!")
     }
 }
+
+#if os(iOS) || targetEnvironment(macCatalyst)
+import UIKit
+
+extension ConfigurableTests {
+    final class LabelView: UIView, Configurable {
+        struct Model {
+            let text: String
+            let fontSize: CGFloat
+        }
+
+        var configurer: (LabelView, Model) -> Void {
+            { view, model in
+                DispatchQueue.main.async {
+                    view.label.text = model.text
+                    view.label.font = UIFont.systemFont(ofSize: model.fontSize)
+                }
+            }
+        }
+
+        private let label = UILabel()
+
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            addSubview(label)
+            label.frame = bounds
+            label.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        }
+
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+
+        static func size(with input: Model?,
+                         constrainedTo parentSize: CGSize?) -> CGSize? {
+            guard let input = input else { return nil }
+            let width = parentSize?.width ?? 100
+            let height = input.text.count > 10 ? 60 : 40
+            return CGSize(width: width, height: CGFloat(height))
+        }
+    }
+
+    func testConfigureUpdatesLabelMainThread() {
+        DispatchQueue.main.async {
+            let view = LabelView()
+            let model = LabelView.Model(text: "Hello", fontSize: 20)
+
+            view.ss.configure(model)
+
+            let label = view.subviews.compactMap { $0 as? UILabel }.first
+            XCTAssertEqual(label?.text, "Hello")
+            XCTAssertEqual(label?.font.pointSize, 20)
+        }
+    }
+
+    func testConfigureUpdatesLabelThread() {
+        let view = LabelView()
+        let model = LabelView.Model(text: "Hello", fontSize: 20)
+
+        view.ss.configure(model)
+
+        let label = view.subviews.compactMap { $0 as? UILabel }.first
+        XCTAssertEqual(label?.text, nil)
+    }
+
+    func testSizeWithModel() {
+        let model = LabelView.Model(text: "Short", fontSize: 14)
+        let parentSize = CGSize(width: 200, height: 100)
+
+        let size = LabelView.size(with: model, constrainedTo: parentSize)
+
+        XCTAssertEqual(size?.width, 200)
+        XCTAssertEqual(size?.height, 40)
+    }
+
+    func testSizeWithLongText() {
+        let model = LabelView.Model(text: "This is a very long string",
+                                    fontSize: 14)
+        let size = LabelView.size(with: model,
+                                  constrainedTo: .init(width: 150, height: 200))
+
+        XCTAssertEqual(size?.height, 60)
+    }
+
+    func testSizeNilModel() {
+        let size = LabelView.size(with: nil, constrainedTo: nil)
+        XCTAssertNil(size)
+    }
+}
+
+#endif
