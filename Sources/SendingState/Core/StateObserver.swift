1cd035582d2998707f4d2be2c424b504e2ffb1c5
Sunny Jeon <dSunny90@users.noreply.github.com>
2020-12-23 23:05:18 +0900

    Refactor StateObserver to use token-based observation

--- Sources/SendingState/Core/StateObserver.swift
+++ Sources/SendingState/Core/StateObserver.swift
@@ -26,17 +26,46 @@ internal final class StateObserver {
     /// The current state value.
     var state: Any?
 
-    /// A closure invoked whenever the observer processes a new state.
-    private var changeHandler: ((Any) -> Void)?
+    private var observers: [UUID: (Any) -> Void] = [:]
 
-    /// Sets a closure to be called whenever the observer's state changes.
-    func setChangeHandler(_ handler: @escaping (Any) -> Void) {
-        changeHandler = handler
+    /// Registers an observer and returns a token that controls its lifetime.
+    ///
+    /// The returned token must be retained for as long as the observation
+    /// should remain active. Releasing or cancelling the token automatically
+    /// removes the observer.
+    ///
+    /// - Parameters:
+    ///   - notifyCurrentState: If `true` and a current state exists, the handler
+    ///                         is called immediately with the current state upon
+    ///                         registration. Defaults to `false`.
+    ///   - handler: A closure invoked whenever a state change occurs.
+    /// - Returns: A ``StateObservationToken`` that manages the lifetime of
+    ///            this observation.
+    @discardableResult
+    func observe(
+        notifyCurrentState: Bool = false,
+        _ handler: @escaping (Any) -> Void
+    ) -> StateObservationToken {
+        let id = UUID()
+        observers[id] = handler
+
+        if notifyCurrentState, let state = state {
+            handler(state)
+        }
+
+        return StateObservationToken { [weak self] in
+            guard let self = self else { return }
+            self.observers[id] = nil
+        }
     }
 
-    /// Notifies the stored change handler closure of a state change.
+    /// Notifies all registered observers of a state change.
+    ///
+    /// - Parameter state: The new state value to deliver to each observer.
     func notifyChange(with state: Any) {
-        changeHandler?(state)
+        for observer in observers.values {
+            observer(state)
+        }
     }
 
     /// Updates the state and triggers configuration .
@@ -47,7 +76,6 @@ internal final class StateObserver {
         binder?.boundState = newState
         configureBlock?(newState)
 
-        // Notify listeners about the incoming state
-        changeHandler?(newState)
+        notifyChange(with: newState)
     }
 }
