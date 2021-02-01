d52864f8acccf3f451bf5c95c9d350d8b6c3298c
Sunny Jeon <dSunny90@users.noreply.github.com>
2021-02-02 00:14:50 +0900

    Expand XCTest coverage for BindableTests

--- Tests/SendingStateTests/ConfigurableTests/BindableTests/BindableTests.swift
+++ Tests/SendingStateTests/ConfigurableTests/BindableTests/BindableTests.swift
@@ -9,4 +9,29 @@ import XCTest
 @testable import SendingState
 
 final class BindableTests: XCTestCase {
+    func testBindableConfiguresBinderCorrectly() {
+        let model = TestModel(contentData: "Hello")
+        let obj = TestObject()
+
+        model.apply(to: obj)
+
+        XCTAssertEqual(obj.inputValue, "Hello")
+    }
+}
+
+extension BindableTests {
+    final class TestObject: Configurable {
+        var inputValue: String?
+        var configurer: (TestObject, String) -> Void = { obj, input in
+            obj.inputValue = input
+        }
+    }
+
+    struct TestModel: Bindable {
+        typealias DataType = String
+        typealias Binder = TestObject
+
+        var contentData: String?
+        var binderType: TestObject.Type { TestObject.self }
+    }
 }
