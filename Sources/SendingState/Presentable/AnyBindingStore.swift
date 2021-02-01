798df460a9c464894e3c1fb1f16552a84257ccbd
Sunny Jeon <dSunny90@users.noreply.github.com>
2021-02-01 19:49:08 +0900

    Add Codable state helpers to BindingStore and AnyBindingStore

--- Sources/SendingState/Presentable/AnyBindingStore.swift
+++ Sources/SendingState/Presentable/AnyBindingStore.swift
@@ -37,6 +37,7 @@ open class AnyBindingStore {
     private let _applyBlock: (Any) -> Void
     private let _observeBlock: (@escaping (Any) -> Void) -> StateObservationToken
     private let _sizeBlock: (CGSize?) -> CGSize?
+    private let _decodeStateBlock: (Data, JSONDecoder) throws -> Any
 
     /// Creates a type-erased wrapper from a concrete `BindingStore`.
     ///
@@ -79,6 +80,8 @@ open class AnyBindingStore {
         _sizeBlock = { size in
             return Binder.size(with: store.state, constrainedTo: size)
         }
+
+        _decodeStateBlock = _AnyBindingStoreDecoderFactory<State>.stateBlock()
     }
 
     /// Applies the underlying store state to the given binder.
@@ -116,6 +119,42 @@ open class AnyBindingStore {
     }
 }
 
+private struct _AnyEncodable: Encodable {
+    private let _encode: (Encoder) throws -> Void
+    init<T: Encodable>(_ value: T) {
+        _encode = { encoder in
+            try value.encode(to: encoder)
+        }
+    }
+    func encode(to encoder: Encoder) throws { try _encode(encoder) }
+}
+
+private struct _AnyBindingStorePayload: Encodable {
+    let state: _AnyEncodable
+    let binderType: String
+}
+
+private struct _AnyBindingStoreDecodablePayload<State: Decodable>: Decodable {
+    let state: State
+    let binderType: String
+}
+
+private enum _AnyBindingStoreDecoderFactory<T> {
+    static func stateBlock() -> (Data, JSONDecoder) throws -> Any {
+        return { _, _ in
+            throw DecodingError.typeMismatch(T.self, .init(codingPath: [], debugDescription: "State does not conform to Decodable."))
+        }
+    }
+}
+
+private extension _AnyBindingStoreDecoderFactory where T: Decodable {
+    static func stateBlock() -> (Data, JSONDecoder) throws -> Any {
+        return { data, decoder in
+            try decoder.decode(T.self, from: data)
+        }
+    }
+}
+
 extension AnyBindingStore: Hashable {
     public static func == (lhs: AnyBindingStore, rhs: AnyBindingStore) -> Bool {
         lhs === rhs
@@ -123,5 +162,79 @@ extension AnyBindingStore: Hashable {
 
     public func hash(into hasher: inout Hasher) {
         hasher.combine(ObjectIdentifier(self))
+    }
+}
+
+public extension AnyBindingStore {
+    /// Encodes the underlying store's current state as a JSON payload.
+    ///
+    /// The resulting object contains two fields:
+    /// - `state`: the encoded state value
+    /// - `binderType`: the type name of the associated binder
+    ///
+    /// - Parameter prettyPrinted: Pass `true` to format the output
+    ///                            for readability. Defaults to `false`.
+    /// - Returns: UTF-8 encoded JSON data.
+    /// - Throws: `EncodingError.invalidValue` if the underlying state
+    ///           does not conform to `Encodable`.
+    func toJSONData(prettyPrinted: Bool = false) throws -> Data {
+        // Attempt to wrap the underlying state as Encodable
+        guard let encodableState = state as? Encodable else {
+            throw EncodingError.invalidValue(state, .init(codingPath: [], debugDescription: "Underlying state does not conform to Encodable."))
+        }
+
+        let payload = _AnyBindingStorePayload(
+            state: _AnyEncodable(encodableState),
+            binderType: String(describing: binderType)
+        )
+        let encoder = JSONEncoder()
+        if prettyPrinted { encoder.outputFormatting.insert(.prettyPrinted) }
+        return try encoder.encode(payload)
+    }
+
+    /// Returns the underlying store's current state as a JSON string.
+    ///
+    /// See ``toJSONData(prettyPrinted:)`` for the payload structure
+    /// and error behavior.
+    ///
+    /// - Parameter prettyPrinted: Pass `true` to format the output
+    ///                            for readability. Defaults to `false`.
+    /// - Returns: A UTF-8 JSON string.
+    /// - Throws: `EncodingError.invalidValue` if the underlying state
+    ///           does not conform to `Encodable`.
+    func toJSONString(prettyPrinted: Bool = false) throws -> String {
+        let data = try toJSONData(prettyPrinted: prettyPrinted)
+        return String(decoding: data, as: UTF8.self)
+    }
+}
+
+public extension AnyBindingStore {
+    /// Decodes a state value from JSON data and updates the underlying store.
+    ///
+    /// - Parameters:
+    ///   - data: UTF-8 encoded JSON representing the underlying state type.
+    ///   - decoder: The decoder to use. Defaults to a new `JSONDecoder`.
+    /// - Throws: A decoding error if `data` cannot be decoded as the
+    ///           underlying state type.
+    func updateState(fromJSONData data: Data,
+                     using decoder: JSONDecoder = JSONDecoder()) throws {
+        let decoded = try _decodeStateBlock(data, decoder)
+        self.state = decoded
+    }
+
+    /// Decodes a state value from a JSON string and updates the underlying store.
+    ///
+    /// See ``updateState(fromJSONData:using:)`` for error behavior.
+    ///
+    /// - Parameters:
+    ///   - jsonString: A UTF-8 JSON string representing the underlying
+    ///                 state type.
+    ///   - decoder: The decoder to use. Defaults to a new `JSONDecoder`.
+    /// - Throws: A decoding error if `jsonString` cannot be decoded as
+    ///           the underlying state type.
+    func updateState(fromJSONString jsonString: String,
+                     using decoder: JSONDecoder = JSONDecoder()) throws {
+        let data = Data(jsonString.utf8)
+        try updateState(fromJSONData: data, using: decoder)
     }
 }
