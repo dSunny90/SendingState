f5fce87e49cecaf4f440125ad2114f26e85ea949
Sunny Jeon <dSunny90@users.noreply.github.com>
2021-03-07 00:06:49 +0900

    Add EventSendable

---
+++ Sources/SendingState/EventSendable/EventSendable.swift
@@ -0,0 +1,38 @@
+//
+//  EventSendable.swift
+//  SendingState
+//
+//  Created by SunSoo Jeon on 06.03.2021.
+//
+
+/// A protocol for types that map UI events to actions and forward them.
+///
+/// Typically, you define `Action` as an enum listing all possible actions
+/// your UI can emit. Use associated values for actions that need additional data
+/// such as user input or sender information.
+///
+/// ### Example:
+/// ```swift
+/// enum MyAction {
+///     case applyMyFilter
+///     case sendReactingLog(String)
+///     case sendClickLog(String)
+///     case textInputChanged(String)
+///     case sliderValueChanged(Float)
+/// }
+/// ```
+///
+public protocol EventSendable {
+    associatedtype Action
+
+    /// Returns the actions for a given sender and event.
+    ///
+    /// Retrieves the actions to forward when a sender triggers an event.
+    /// Returns an empty array if no matching mapping exists.
+    ///
+    /// - Parameters:
+    ///   - sender: The sender object that triggered the event.
+    ///   - event: The event that occurred.
+    /// - Returns: An array of actions to forward.
+    func actions(for sender: AnyObject, event: SenderEvent) -> [Action]
+}
