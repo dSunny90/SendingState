098811b108d53d883b86c017cf4b334594f05de6
Sunny Jeon <dSunny90@users.noreply.github.com>
2021-03-13 08:21:22 +0900

    Add EventForwarder for EventSendable

---
+++ Sources/SendingState/EventSendable/EventForwarder.swift
@@ -0,0 +1,27 @@
+//
+//  EventForwarder.swift
+//  SendingState
+//
+//  Created by SunSoo Jeon on 13.03.2021.
+//
+
+/// A type that maps events from a specific sender to actions.
+///
+/// `EventForwarder` captures a sender and defines mappings from its events
+/// to the actions forwarded when those events occur.
+///
+/// Use `EventForwarder` to define event behavior for a specific sender.
+public struct EventForwarder<Action> {
+    /// The sender whose events to forward.
+    public let sender: AnyObject
+
+    /// The mappings from sender events to their corresponding action providers.
+    /// Closures are stored to enable lazy evaluation at event time,
+    /// allowing actions to capture real-time sender state.
+    public let mappings: [SenderEvent: () -> [Action]]
+
+    /// Returns the actions mapped to the given event.
+    public func actions(for event: SenderEvent) -> [Action] {
+        mappings[event]?() ?? []
+    }
+}
