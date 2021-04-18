50fc82d9e04b51e91afcf5a557cf0051457eaeae
Sunny Jeon <dSunny90@users.noreply.github.com>
2021-04-19 00:52:05 +0900

    Add attach/detach helpers to AnyActionHandlingProvider

--- Sources/SendingState/EventSendable/ActionHandlingProvider/AnyActionHandlingProvider.swift
+++ Sources/SendingState/EventSendable/ActionHandlingProvider/AnyActionHandlingProvider.swift
@@ -32,3 +32,28 @@ public final class AnyActionHandlingProvider {
         _handle(action)
     }
 }
+
+#if os(iOS) || targetEnvironment(macCatalyst)
+import UIKit
+
+extension AnyActionHandlingProvider {
+    /// Attaches this handler to the view so it receives the view's
+    /// forwarded events.
+    ///
+    /// - Parameter view: The view whose events this handler will receive.
+    public func attach<V: UIView & EventSendingProvider>(to view: V) {
+        view.ss.addAnyActionHandler(to: self)
+    }
+
+    /// Detaches this handler from the view, removing all event bindings
+    /// that were previously established via ``attach(to:)``.
+    ///
+    /// After detaching, the view's events will no longer be forwarded
+    /// to this handler.
+    ///
+    /// - Parameter view: The view to stop receiving events from.
+    public func detach<V: UIView & EventSendingProvider>(from view: V) {
+        view.ss.removeAnyActionHandler(from: self)
+    }
+}
+#endif
