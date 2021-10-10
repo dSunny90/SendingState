c93c50b0d2231443125bcd909271b8d701df3e9a
Sunny Jeon <dSunny90@users.noreply.github.com>
2021-10-10 15:57:00 +0900

    SenderEventBox UIKit Support - Add Source

---
+++ Sources/SendingState/EventSendable/PlatformSupport/iOS/UIControlSenderEventBox.swift
@@ -0,0 +1,56 @@
+//
+//  UIControlSenderEventBox.swift
+//  SendingState
+//
+//  Created by SunSoo Jeon on 10.10.2021.
+//
+
+#if os(iOS) || targetEnvironment(macCatalyst)
+import UIKit
+
+/// A wrapper that connects a UIControl event to an action handler closure.
+///
+/// This box is retained by a memory pool and releases the closure and target
+/// registration during cleanup to break retain cycles.
+internal final class UIControlSenderEventBox: SenderEventBox<UIControl> {
+    /// Weak reference to the control to avoid retain cycle.
+    private weak var control: UIControl?
+
+    /// The specific UIControl event being handled.
+    private let event: UIControl.Event
+
+    /// Initializes the box and registers the action for the control event.
+    ///
+    /// - Parameters:
+    ///   - control: The UIControl to attach the action to.
+    ///   - events: The event to handle (e.g., .touchUpInside).
+    ///   - actionHandler: The closure to invoke on event.
+    @inlinable internal init(
+        control: UIControl,
+        on events: UIControl.Event,
+        actionHandler: @escaping (UIControl) -> Void
+    ) {
+        self.control = control
+        self.event = events
+        super.init(actionHandler)
+        DispatchQueue.main.async {
+            control.addTarget(
+                self, action: #selector(self.invoke(_:)), for: self.event
+            )
+        }
+    }
+
+    /// Cleans up the target and removes strong references to avoid leaks.
+    ///
+    /// Called by memory pool or manually when the lifecycle ends.
+    override func cleanup() {
+        DispatchQueue.main.async {
+            self.control?.removeTarget(
+                self, action: #selector(self.invoke(_:)), for: self.event
+            )
+            self.control = nil
+        }
+        super.cleanup()
+    }
+}
+#endif
---
+++ Sources/SendingState/EventSendable/PlatformSupport/iOS/UIGestureRecognizerSenderEventBox.swift
@@ -0,0 +1,66 @@
+//
+//  UIGestureRecognizerSenderEventBox.swift
+//  SendingState
+//
+//  Created by SunSoo Jeon on 10.10.2021.
+//
+
+#if os(iOS) || targetEnvironment(macCatalyst)
+import UIKit
+
+/// A wrapper that connects a UIGestureRecognizer to an action handler closure.
+///
+/// This box is retained by a memory pool and releases the closure and target
+/// registration during cleanup to break retain cycles.
+internal final class UIGestureRecognizerSenderEventBox<T: UIGestureRecognizer>
+    : SenderEventBox<T>
+{
+    /// Weak reference to the gesture recognizer to avoid retain cycle.
+    private weak var recognizer: UIGestureRecognizer?
+
+    /// Allowed states to invoke the handler (e.g., [.recognized]).
+    private var allowedStates: Set<UIGestureRecognizer.State> = []
+
+    /// Initializes the box and registers the gesture recognizer.
+    ///
+    /// - Parameters:
+    ///   - recognizer: The gesture recognizer to observe.
+    ///   - states: Gesture states that trigger the action.
+    ///   - actionHandler: The closure to invoke on gesture event.
+    @inlinable internal init(
+        recognizer: T,
+        on states: Set<UIGestureRecognizer.State> = [.recognized],
+        actionHandler: @escaping (T) -> Void
+    ) {
+        self.allowedStates = states
+        self.recognizer = recognizer
+        super.init(actionHandler)
+        DispatchQueue.main.async {
+            recognizer.addTarget(self, action: #selector(self.invoke(_:)))
+        }
+    }
+
+    /// Called by the system when the gesture is triggered.
+    @objc override func invoke(_ sender: Any) {
+        DispatchQueue.main.async {
+            guard let recognizer = sender as? T else { return }
+            guard
+                self.allowedStates.isEmpty ||
+                self.allowedStates.contains(recognizer.state)
+            else { return }
+            self.box?(recognizer)
+        }
+    }
+
+    /// Cleans up gesture recognizer registration and memory references.
+    override func cleanup() {
+        DispatchQueue.main.async {
+            self.recognizer?.removeTarget(
+                self, action: #selector(self.invoke(_:))
+            )
+            self.recognizer = nil
+        }
+        super.cleanup()
+    }
+}
+#endif
